08d177a63b9a619a482bdffa6baca7ad8f33f0dc
Patrick Gustaevel <loft45@gmail.com>
2016-12-06 14:42:15 -0500

    more menu changes

--- extension.js
+++ extension.js
@@ -37,7 +37,6 @@ if (typeof(window.sessionStorage) === "undefined") {
     modules.uiScriptMenu.enable();
     modules.uiChartMenu.enable();
     modules.uiNotes.enable();
-    modules.uiTimers.enable();
     modules.uiCustomTimer.enable();
     modules.uiActionShortcuts.enable();
 }
--- src/ui/scriptMenu.js
+++ src/ui/scriptMenu.js
@@ -5,26 +5,17 @@
 
     function setupMenu(template) {
         $('#navWrapper').prepend($(template));
+
+        $('#roaMenuTitle').text(GM_info.script.name + " " + GM_info.script.version);
     }
 
     module.enable = function () {
 
-
         $.get(modules.urls.html.script_menu).done(setupMenu);
-        // script_menu
 
-        /*var $helpSection = $("#helpSection");
-
-        // Script menu button
-        var $menuLink = $('<a id="roaMenu" href="javascript:;"/>')
-            .html('<li class="active">' + GM_info.script.name + " " + GM_info.script.version + '</li>');
-        //.click(); TODO
-
-        $helpSection.prepend($menuLink);
-
-        this.enabled = true;*/
     };
 
     modules.uiScriptMenu = module;
+    modules.uiScriptMenu.enable();
 
 })(modules.jQuery);
